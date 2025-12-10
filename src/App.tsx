8f1563257ad3d2ee5cbb907ca7414e81e6f64ad3
zsuczko <instabb6464@gmail.com>
2025-12-10 09:33:03 +0100

    Alapok kész

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
+import { createContext, useEffect, useState } from "react";
 import Card from "./components/Card";
 
+type CardType = {
+  question: string;
+  answer: string;
+  points: number;
+};
+
+export const MainContext = createContext<{
+  place: number;
+  setPlace: (szam: number) => void;
+}>({
+  place: 1,
+  setPlace: () => {},
+});
+
 function App() {
+  const [cards, setCards] = useState<CardType[]>([]);
+
+  const [place, setPlace] = useState<number>(1);
+
+  useEffect(() => {
+    // setCards([]);
+    fetch("/cards.json")
+      .then((res) => res.json())
+      .then((items) => {
+        setCards(items);
+      });
+  }, []);
+
   return (
     <>
-      <main className="bg-linear-to-r from-bg-color-1 to-bg-color-2 w-screen h-screen flex justify-center items-center">
-        <Card></Card>
+      <MainContext.Provider value={{ place, setPlace }}>
+        <main className="bg-linear-to-r from-bg-color-1 to-bg-color-2 w-screen h-screen flex justify-center items-center">
+          <Card {...cards[place - 1]}></Card>
 
-        <button className="bg-white p-5 text-xl font-bold rounded-2xl absolute bottom-30 right-2">
-          Új gyakorlás indítása
-        </button>
-        <button className="bg-white p-5 text-xl font-bold rounded-2xl absolute bottom-10 right-2">
-          Új kártya hozzáadása
-        </button>
-      </main>
+          <button
+            className="bg-white p-5 text-xl font-bold rounded-2xl absolute bottom-30 right-2"
+            onClick={() => {
+              setPlace(1);
+            }}
+          >
+            Új gyakorlás indítása
+          </button>
+          <button className="bg-white p-5 text-xl font-bold rounded-2xl absolute bottom-10 right-2">
+            Új kártya hozzáadása
+          </button>
+        </main>
+      </MainContext.Provider>
     </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,15 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { MainContext } from "../App";
 
-const Card = () => {
+const Card = (prop: { points: number; question: string; answer: string }) => {
   const [flipped, setFlipped] = useState<boolean>(true);
+
+  const { place, setPlace } = useContext(MainContext);
+
+  function nextPlace() {
+    if (place < 12) setPlace(place + 1);
+  }
+
   return (
     <div className="card-wrapper">
       <div
@@ -10,11 +18,48 @@ const Card = () => {
           setFlipped((prev) => !prev);
         }}
       >
-        <div className="w-[20em] h-[15em] bg-green-en text-white flex justify-center items-center front">
-          Mi a különbség?
+        <div
+          className={`w-[25em] h-[20em] ${
+            prop.points > 0
+              ? "bg-green-en"
+              : prop.points === 0
+              ? "bg-white"
+              : "bg-red-en"
+          } ${
+            prop.points === 0 ? "text-black" : "text-white"
+          } flex justify-center items-center front`}
+        >
+          {prop.question}
         </div>
-        <div className="w-[20em] h-[15em] bg-green-en text-white flex justify-center items-center back">
-          Másik oldal
+        <div className="w-[25em] h-[20em] bg-white flex justify-between items-center back relative flex-col pt-16 pb-10 pr-4 pl-4">
+          <div className="absolute w-full bg-green-en top-0 left-0  flex justify-center p-1 font- text-white">
+            {place}
+          </div>
+          <div className="text-center font-bold">{prop.answer}</div>
+          <div className="flex justify-between gap-20">
+            <div className="flex flex-col gap-3 items-center">
+              <p className="text-green-en">Sikerült</p>
+              <button
+                className="bg-green-en rounded-[50%] text-white w-16 h-16 flex justify-center items-center text-5xl"
+                onClick={() => {
+                  nextPlace();
+                }}
+              >
+                <i className="fa-regular fa-circle-check"></i>
+              </button>
+            </div>
+            <div className="flex flex-col gap-3 items-center">
+              <p className="text-red-en">Nem sikerült</p>
+              <button
+                className="bg-red-en rounded-[50%] text-white w-16 h-16 flex justify-center items-center text-5xl"
+                onClick={() => {
+                  nextPlace();
+                }}
+              >
+                <i className="fa-regular fa-circle-xmark"></i>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
